28e9b870b393a380a89ecee212ef7a7c97602866
bilalb53 <bilalb53@gmail.com>
2017-03-07 17:31:04 +0500

    Testing Github in Global.asax.cs

--- SEA_Application/SEA_Application/Global.asax.cs
+++ SEA_Application/SEA_Application/Global.asax.cs
@@ -16,6 +16,7 @@ namespace SEA_Application
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //hello testing github
         }
     }
 }
